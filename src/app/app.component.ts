87ca94d37cb4753f4c4e61b887f54f237d4c2292
Jhonatha1 <yankyboa@gmail.com>
2023-12-27 14:21:13 -0300

    Renderização condicional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from "./components/first-component/first-component.component";
 import { ParentDataComponent } from "./components/parent-data/parent-data.component";
 import { DirectivesComponent } from "./components/directives/directives.component";
-
+import { IfRenderComponent} from "./components/if-render/if-render.component";
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, FirstComponentComponent, ParentDataComponent, DirectivesComponent]
+    imports: [CommonModule, RouterOutlet, FirstComponentComponent, ParentDataComponent, DirectivesComponent, IfRenderComponent]
 })
 export class AppComponent {
   title = 'curso-angular';
