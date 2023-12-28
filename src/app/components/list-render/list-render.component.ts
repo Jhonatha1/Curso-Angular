b6bf669653c12cdcee3651f01acd003912ce9b9f
Jhonatha1 <yankyboa@gmail.com>
2023-12-28 14:54:22 -0300

    Services do Angular

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Animal } from '../../Animal';
 
+
+import { ListService } from '../../services/list.service';
 @Component({
   selector: 'app-list-render',
   standalone: true,
@@ -19,7 +21,15 @@ export class ListRenderComponent {
 
     animalDetails = ''
 
+    constructor(private listService: ListService){}
+
+
     showAge(animal:Animal){
       this.animalDetails = `'O pet' ${animal.name} tem ${animal.age} anos de idade.`
+    }
+
+    removeAnimal(animal: Animal){
+      console.log('Removendo animal');
+      this.animals = this.listService.remove(this.animals, animal);
     }
 }
